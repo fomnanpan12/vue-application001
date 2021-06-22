fd8caf7246651f17968e3ca4d753a4cf6c2fe4f6
Fom Nanpan Wilson <79115812+fomnanpan12@users.noreply.github.com>
2021-06-22 11:26:25 +0100

    Add files via upload

---
+++ script.vue
@@ -0,0 +1,11 @@
+var app4 = new Vue({
+    el:'#app-4',
+    data: {
+        books: [
+            {staff: 'Nanpan', subjects: 'Biology'},
+            {staff: 'Wilson', subjects: 'Chemistry'},
+            {staff: 'Fom', subjects: 'Physics'},
+
+        ]
+    }
+})
